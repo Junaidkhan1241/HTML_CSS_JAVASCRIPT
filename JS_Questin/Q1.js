8915dfae30bdcb3aff12a77e8e9e80176f27ea64
Junaid khan <165429906+Junaidkhan1241@users.noreply.github.com>
2024-04-05 13:43:02 -0700

    JS Questions

---
+++ JS_Questin/Q1.js
@@ -0,0 +1,3 @@
+let n=5;
+let p=n++; // p=5 and n is incremnt by 1
+console.log(p,n);   //  ans = 5 6 //
---
+++ JS_Questin/Q2.js
@@ -0,0 +1,4 @@
+//How to combine 2 Arrays together
+let arr1 = [1, 2, 3, 4];
+let arr2 =[5, 6, 7, 8];
+console.log(...arr1  , ... arr2);
