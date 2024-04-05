12579ace11e382856ae49696707070c77d2c87fe
Junaid khan <165429906+Junaidkhan1241@users.noreply.github.com>
2024-04-05 02:54:21 -0700

    Simple Todo List

---
+++ Todo List/script.js
@@ -0,0 +1,22 @@
+// Junaid khan front end developer
+document.getElementById("btn").addEventListener("click", add);
+document.getElementById("btn").addEventListener("click", two);
+
+function add() {
+    let value = document.getElementById("input").value;
+
+    let li = document.createElement('li');
+    li.textContent = value;
+
+    //   document.getElementById("result").innerText=value;
+    document.getElementById("result").appendChild(li);
+
+}
+
+function two() {
+    document.getElementById("result").style.background = "#ccc"
+    document.getElementById("result").style.color = "#000"
+}
+
+
+
